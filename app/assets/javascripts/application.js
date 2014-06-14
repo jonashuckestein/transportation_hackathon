e569fbe7fd4ca5602cae59b7adf4df572e8e31eb
Jonas Huckestein <jonas.huckestein@gmail.com>
2014-06-14 16:19:43 +0200

    remove cruft

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -4,13 +4,4 @@
 //= require turbolinks
 //= require bootstrap
 //= require vendor/fastclick.min
-//= require vendor/underscore.min
-//= require vendor/backbone.min
-//= require vendor/nprogress
-//
 //= require transportation
-//= require remote_forms
-//= require remote_modals
-//
-//= require_tree ./util
-//= require_tree ./views
